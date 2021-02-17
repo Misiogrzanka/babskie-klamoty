7f368d2f5206f88ee4e5e26b3c58c4b30495f623
Misiogrzanka <77200161+Misiogrzanka@users.noreply.github.com>
2021-02-17 17:21:41 +0100

    Re-design onwards (Atomic design, layout changes)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { Footer, Header, Sidenav } from '../src/Components/Static/Layout';
-import Layout from '../src/Components/Static/Layout.js'
-
 import './sass/App.scss';
 
 function App() {
 	return (
-		<Layout />
+		<div class="App">
+		</div>
 	);
 }
 
